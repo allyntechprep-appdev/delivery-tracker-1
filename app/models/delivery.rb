b9af8ef68ac3409711d219d58f18e3120018bda8
allyntechprep <119367002+allyntechprep@users.noreply.github.com>
2023-05-11 17:29:45 +0000

    home page progress

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id         :integer          not null, primary key
+#  content    :string
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 class Delivery < ApplicationRecord
 end
